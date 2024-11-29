2050dcbc0ffc00cdff036ac71da4f49191f4f218
Babalpreet <babaldon1111@gmail.com>
2024-11-29 16:05:44 +0530

    PropsLearning

---
+++ Chai_or_code/03tailwindprops/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './Card'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const data ={
+    price: 1000,
+    address: "jalandhar cantt",
+  }
+  const data2 ={
+    price: 21000,
+    address: "ambala cantt",
+  }
+  return (
+    <>
+      {/* <h1 className='bg-green-400'p-4 rounded-xl>Tilwind test</h1>
+      */}
+      <Card data={data} />
+      <Card data={data2} />
+
+    </>
+  )
+}
+
+export default App
