35ba592c1d8479f09e8d828fe36ee041cbbb0332
Babal1111 <babaldon1111@gmail.com>
2025-06-04 22:52:57 +0530

    dom revising

--- Js_practising/MethodsAndFunction.js
+++ Js_practising/MethodsAndFunction.js
@@ -81,3 +81,19 @@ console.log(result3);
 // or
 let result4 = studens.filter( stu => stu.marks>90).map(stu => stu.name);
 console.log(result4);
+
+
+let num = [1,2,3,4,5];
+console.log(num);
+num.splice(2);
+
+console.log(num);
+
+
+let hi = (a,b)=>{
+    console.log("this is a type of function");
+    console.log(a+b);
+}
+
+hi(10,20);
+console.log(hi);
