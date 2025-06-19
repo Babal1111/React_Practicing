076ab4525e59604236b35fbb45024de4d91c80ff
Babal1111 <babaldon1111@gmail.com>
2025-06-19 10:16:18 +0530

    Lauout
    Made layout, wrapped ements in it, made footer, and header

--- pep_react/pep_react/src/App.js
+++ pep_react/pep_react/src/App.js
@@ -1,14 +1,14 @@
  import {Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
-
+import AppLayout from "./layout/AppLayout";
 function App() {
   return (
     <>
    
     <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/" element={<AppLayout><Home/></AppLayout>}></Route>
+      <Route path="/login" element={<AppLayout><Login/></AppLayout>}></Route>
 
     </Routes>
     </>
---
+++ pep_react/pep_react/src/layout/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <>
+      <div className="container-fluid textcenter">
+        <p>I am Footer!</p>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
